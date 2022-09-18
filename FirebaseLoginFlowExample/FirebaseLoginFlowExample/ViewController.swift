6227525c50145ca0fafeca7cb8b8752ef07a54be
Roy Groot Hulze <info@roygroothulze.nl>
2022-09-18 17:09:22 +0200

    Added example

---
+++ FirebaseLoginFlowExample/FirebaseLoginFlowExample/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FirebaseLoginFlowExample
+//
+//  Created by Roy Groot Hulze on 18/09/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
